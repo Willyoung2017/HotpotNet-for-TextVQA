b63bcba8382c622c9f2cc0d57a916ff253adac93
sherryzyh <sherryyh.z@gmail.com>
2022-03-31 11:13:36 -0400

    gradebook (not)require reading subset

--- Model-Analysis/read_gradebook.py
+++ Model-Analysis/read_gradebook.py
@@ -1,10 +1,20 @@
 import pickle
 import pandas as pd
+import argparse
 
-df = pd.read_csv('gradebooks/grade_book.csv')
-# print(df)
+df = pd.read_csv('gradebooks/grade_book_whole.csv')
 
-for subset_name in ["position", "obj_related_question"]:
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--SUBSETS', '-s',
+					help='subset names splitted with comma',
+					required=True)
+
+args = parser.parse_args()
+subset_list = args.SUBSETS.split(",")
+print(subset_list)
+
+for subset_name in subset_list:
 	subset_path = "subsets/val_" + subset_name + ".txt"
 	with open(subset_path, "r") as f:
 		subset = f.read()
@@ -18,9 +28,9 @@ for subset_name in ["position", "obj_related_question"]:
 	# 	'accu_hotpot-without-object-label', 'accu_hotpot', 'accu_object',
 	# 	'accu_ocr', 'accu_question', 'accu_lorra', 'accu_m4c', 'accu_tap']])
 
-	subset_df.to_csv("gradebooks/"+subset_name+"_grade_book.csv")
+	subset_df.to_csv("gradebooks/grade_book_"+subset_name+".csv")
 
 # read model_preds
-with open("model_preds.pickle", "rb") as f:
-	model_preds = pickle.load(f)
+# with open("model_preds.pickle", "rb") as f:
+# 	model_preds = pickle.load(f)
 # print(model_preds)
