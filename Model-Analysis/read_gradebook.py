e99f3e2775906e71d7a2e814f25e0e1c99370757
sherryzyh <sherryyh.z@gmail.com>
2022-05-04 20:52:11 -0400

    generate and analyze subset gradebook

--- Model-Analysis/read_gradebook.py
+++ Model-Analysis/read_gradebook.py
@@ -9,14 +9,19 @@ def main():
 	parser.add_argument('--SUBSETS', '-s',
 						help='subset names splitted with comma',
 						required=True)
-
 	args = parser.parse_args()
 	subset_list = args.SUBSETS.split(",")
 	print(subset_list)
 
 	df = pd.read_csv('gradebooks/grade_book_whole.csv')
+	subset_statistics = dict()
 	for subset_name in subset_list:
-		generate_gradebook(subset_name, df)
+		subset_df = generate_gradebook(subset_name, df)
+		subset_accu = analyze_gradebook(subset_df)
+		subset_statistics[subset_name] = subset_accu
+	
+	subset_performance_df = pd.DataFrame(subset_statistics)
+	print(subset_performance_df)
 
 def generate_gradebook(subset_name, df):
 	subset_path = "subsets/val_" + subset_name + ".txt"
@@ -34,7 +39,17 @@ def generate_gradebook(subset_name, df):
 	# 	'accu_ocr', 'accu_question', 'accu_lorra', 'accu_m4c', 'accu_tap']])
 
 	subset_df.to_csv("gradebooks/grade_book_"+subset_name+".csv")
+	return subset_df
 
+def analyze_gradebook(subset_df):
+	performance_dict = dict()
+	model_list = ['accu_hotpot-lorra', 'accu_hotpot-without-mmt',
+		'accu_hotpot-without-object-label', 'accu_hotpot', 'accu_object',
+		'accu_ocr', 'accu_question', 'accu_lorra', 'accu_m4c', 'accu_tap']
+	for m in model_list:
+		performance_dict[m] = subset_df[m].sum()/len(subset_df)
+	
+	return performance_dict
 
 if __name__ == '__main__':
 	main()
